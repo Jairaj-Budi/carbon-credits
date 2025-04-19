3b42c769da101baaafd7a1c74182c18483e31391
Jairaj Budi <jairajbudi678@gmail.com>
2025-04-20 02:12:14 +0530

    fix: deployment

--- server/vite.ts
+++ server/vite.ts
@@ -26,7 +26,6 @@ export async function setupVite(app: Express, server: Server) {
   const serverOptions = {
     middlewareMode: true,
     hmr: { server },
-    allowedHosts: true,
   };
 
   const vite = await createViteServer({
@@ -71,7 +70,7 @@ export async function setupVite(app: Express, server: Server) {
 }
 
 export function serveStatic(app: Express) {
-  const distPath = path.resolve(__dirname, "public");
+  const distPath = path.resolve(__dirname, "..", "dist", "public");
 
   if (!fs.existsSync(distPath)) {
     throw new Error(
